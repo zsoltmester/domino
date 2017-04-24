11629144bd267e1f0cd79ed8f4b88b0db36b3377
zsmester <zsolt.mester67@gmail.com>
2017-04-24 21:49:51 +0200

    Fix the test runner.

---
+++ domino/DominoClient.java
@@ -0,0 +1,9 @@
+package domino;
+
+public class DominoClient {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
---
+++ domino/DominoServer.java
@@ -0,0 +1,9 @@
+package domino;
+
+public class DominoServer {
+
+    public static void main(String[] args) {
+
+    }
+
+}
